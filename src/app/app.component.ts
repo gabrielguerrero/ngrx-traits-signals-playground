3164201d4048967dd42fd0f951246af918ecf46e
Gabriel Guerrero <gabriel.issa@medivet.co.uk>
2024-05-31 23:51:01 +0100

    make infinite scroll main component to render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListDetailLocalComponent } from './product-list-detail/product-list-detail-local.component';
 import { ProductListDetailRemoteComponent } from './product-list-detail/product-list-detail-remote.component';
+import { ProductBranchListInfiniteComponent } from './product-list-detail/product-branch-list-infinite.component';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,9 @@ import { ProductListDetailRemoteComponent } from './product-list-detail/product-
     RouterOutlet,
     ProductListDetailLocalComponent,
     ProductListDetailRemoteComponent,
+    ProductBranchListInfiniteComponent,
   ],
-  template: '<product-list-detail-local/>',
+  template: '<product-branch-infinite-list/>',
 })
 export class AppComponent {
   title = 'ngrx-traits-signals-playground';
